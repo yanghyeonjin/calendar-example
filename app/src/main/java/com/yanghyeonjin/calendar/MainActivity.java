378a4369cfa5a7d93877d4af11033297c2627c55
yanghyeonjin <guswls7081@gmail.com>
2020-07-07 16:58:42 +0900

    * 캘린더 라이브러리 import * 기본 달력 show

--- app/src/main/java/com/yanghyeonjin/calendar/MainActivity.java
+++ app/src/main/java/com/yanghyeonjin/calendar/MainActivity.java
@@ -2,13 +2,27 @@ package com.yanghyeonjin.calendar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 
 public class MainActivity extends AppCompatActivity {
+    private MaterialCalendarView calendarView;
+
+    private static final String TAG = "MainActivity";
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        context = MainActivity.this;
+
+
+        calendarView = findViewById(R.id.calendarView);
+        Toast.makeText(context, String.valueOf(calendarView.getSelectedDates()), Toast.LENGTH_SHORT).show();
     }
 }
