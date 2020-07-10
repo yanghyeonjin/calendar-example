c3c6c3da036a42a3e3352432b60d7eab12ccc19b
yanghyeonjin <guswls7081@gmail.com>
2020-07-10 11:39:03 +0900

    * 체크 상태인 아이템 맨 아래로 이동

--- app/src/main/java/com/yanghyeonjin/calendar/weekcalendar/TodoAdapter.java
+++ app/src/main/java/com/yanghyeonjin/calendar/weekcalendar/TodoAdapter.java
@@ -15,6 +15,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.yanghyeonjin.calendar.R;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class TodoAdapter extends RecyclerView.Adapter<TodoAdapter.TodoViewHolder> {
     private ArrayList<Todo> todoList;
@@ -63,6 +64,25 @@ public class TodoAdapter extends RecyclerView.Adapter<TodoAdapter.TodoViewHolder
                 public void onClick(View view) {
                     Intent todoIntent = new Intent(context, TodoDetailActivity.class);
                     context.startActivity(todoIntent);
+                }
+            });
+
+
+
+            // checkbox 클릭했을 때, 제일 아래로 이동
+            checkTodo.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (((CheckBox) view).isChecked()) {
+                        // 체크 되어 있으면 아래로 이동
+                        int curPos = getAdapterPosition();
+                        Todo completeTodo = todoList.get(curPos); // 클릭한 아이템
+                        completeTodo.setComplete(true); // 체크 상태로 변경
+
+                        todoList.remove(todoList.get(curPos)); // 해당 위치에 아이템 삭제하고
+                        todoList.add(todoList.size(), completeTodo); // 맨 아래에 추가
+                        notifyDataSetChanged();
+                    }
                 }
             });
         }
