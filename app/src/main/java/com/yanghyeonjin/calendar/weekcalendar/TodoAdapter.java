6cb91161acc220c9487ff8db42e5eaca97a55a96
yanghyeonjin <guswls7081@gmail.com>
2020-07-10 13:44:51 +0900

    * 체크박스 상태에 따라 아이템 재정렬하기

--- app/src/main/java/com/yanghyeonjin/calendar/weekcalendar/TodoAdapter.java
+++ app/src/main/java/com/yanghyeonjin/calendar/weekcalendar/TodoAdapter.java
@@ -2,14 +2,19 @@ package com.yanghyeonjin.calendar.weekcalendar;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.yanghyeonjin.calendar.R;
@@ -35,9 +40,17 @@ public class TodoAdapter extends RecyclerView.Adapter<TodoAdapter.TodoViewHolder
     }
 
     @Override
-    public void onBindViewHolder(@NonNull TodoAdapter.TodoViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull final TodoAdapter.TodoViewHolder holder, int position) {
         holder.checkTodo.setChecked(todoList.get(position).isComplete());
         holder.tvTodo.setText(todoList.get(position).getTodoTitle());
+
+
+        if (todoList.get(position).isComplete()) {
+            // 체크되어 있으면 배경색 바꾸기
+            holder.vBlur.setBackgroundColor(ContextCompat.getColor(context, R.color.completeTodoBg));
+        } else {
+            holder.vBlur.setBackgroundColor(ContextCompat.getColor(context, R.color.transparent));
+        }
     }
 
     @Override
@@ -49,13 +62,30 @@ public class TodoAdapter extends RecyclerView.Adapter<TodoAdapter.TodoViewHolder
         private CheckBox checkTodo;
         private TextView tvTodo;
         private CardView todo;
+        private View vBlur;
 
         public TodoViewHolder(@NonNull View itemView) {
             super(itemView);
 
+
+            // 아이디 연결
             checkTodo = itemView.findViewById(R.id.checkTodo);
             tvTodo = itemView.findViewById(R.id.tvTodo);
             todo = itemView.findViewById(R.id.todo);
+            vBlur = itemView.findViewById(R.id.vBlur);
+
+
+            // blur 뷰 높이 설정
+            todo.post(new Runnable() {
+                @Override
+                public void run() {
+                    int h = todo.getHeight(); // to do 박스의 높이 계산
+
+                    ViewGroup.LayoutParams params = vBlur.getLayoutParams();
+                    params.height = h;
+                    vBlur.setLayoutParams(params);
+                }
+            });
 
 
             // 할 일 클릭
@@ -69,22 +99,32 @@ public class TodoAdapter extends RecyclerView.Adapter<TodoAdapter.TodoViewHolder
 
 
 
-            // checkbox 클릭했을 때, 제일 아래로 이동
+            // 체크박스 클릭했을 때, 상태에 따라 아이템 position 이동하기
             checkTodo.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    if (((CheckBox) view).isChecked()) {
-                        // 체크 되어 있으면 아래로 이동
-                        int curPos = getAdapterPosition();
-                        Todo completeTodo = todoList.get(curPos); // 클릭한 아이템
-                        completeTodo.setComplete(true); // 체크 상태로 변경
+                    int curPos = getAdapterPosition();
 
-                        todoList.remove(todoList.get(curPos)); // 해당 위치에 아이템 삭제하고
-                        todoList.add(todoList.size(), completeTodo); // 맨 아래에 추가
-                        notifyDataSetChanged();
+                    if (((CheckBox) view).isChecked()) {
+                        // 체크 되었을 때
+                        // 체크한 아이템 복사해서 isComplete만 바꾼 새로운 아이템 생성
+                        Todo todoBefore = todoList.get(curPos);
+                        Todo todoAfter = new Todo(true, todoBefore.getTodoTitle());
+
+                        // 해당 포지션에 있는 아이템 삭제 후, 맨 아래에 데이터 추가 -> 아래로 이동한 것처럼 보이도록
+                        todoList.remove(todoBefore);
+                        todoList.add(todoAfter);
+
+                        // 아이템 remove, add 알리기
+                        notifyItemRemoved(curPos);
+                        notifyItemInserted(getItemCount() - 1);
+                    } else {
+                        todoList.get(curPos).setComplete(false);
+                        notifyItemChanged(curPos); // 아이템의 데이터가 변경되었음을 알림.
                     }
                 }
             });
+
         }
     }
 }
